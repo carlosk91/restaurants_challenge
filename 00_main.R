a5fbfc0cb2f595d4c318bfe3f2cb6e1c8b913285
carlosk91 <carlosk0891@gmail.com>
2021-08-27 11:15:55 -0500

    20210827 - Commenting main.R

--- 00_main.R
+++ 00_main.R
@@ -10,7 +10,7 @@ source('02_functions.R') # Required functions
 source('03_data_push.R') # Pushing csvs to MySQL local DB
 source('04_data_read.R') # Creating queries to answer questions 1, 2, and 5
 source('05_data_analytics.R') # Script to answer the challenge
-render('06_mdr_creation.Rmd')
+render('06_mdr_creation.Rmd') # Script to create the markdown
 
 h2o.init()
 # h2o.clusterInfo()
